cdba091249417ba2cc8460f469aa06d41fd51444
mirza2018 <mirza.eee.4th@gmail.com>
2024-10-19 13:06:57 +0600

    Sign up page done

--- components/SignUp/SignUp.jsx
+++ components/SignUp/SignUp.jsx
@@ -1,7 +1,7 @@
 import Image from 'next/image';
 import React from 'react';
 import { IoMdArrowBack } from "react-icons/io";
-import loginImg from "../../asserts/login-img.png";
+import loginImg from "../../asserts/Signup-img.png";
 import logo from "../../asserts/logo.png";
 import { FcGoogle } from "react-icons/fc";
 
@@ -9,30 +9,56 @@ import { FcGoogle } from "react-icons/fc";
 const SignUp = () => {
     return (
         <div className='container mx-auto pt-5'>
-            {/* Login Back button */}
+            {/* sign in Back button */}
             <div className='flex flex-shrink-0 items-center text-xl text-[#656565] gap-4 font-bold'>
                 <IoMdArrowBack className='text-3xl' />
                 <h1 className=''>Back To Home</h1>
             </div>
             {/* Main content */}
-            <div className='flex gap-8'>
-                {/* Left side Image */}
+            <div className='flex gap-8 flex-row-reverse'>
+                {/* Right side Image */}
                 <div className='flex flex-col justify-end mt-10 w-1/2 '>
                     <Image alt='dog-Image' src={loginImg} className='object-cover aspect-square rounded shadow-xl' />
                 </div>
 
-                {/* Right side login functionality */}
+                {/* Left side signup functionality */}
                 <div className='flex-1 flex flex-col justify-center items-center '>
                     <div className='mb-5 '>
                         <Image alt='dog-Image' src={logo} className='w-52 ' />
                     </div>
-                    <h1 className='text-4xl font-bold mb-2 mt-16'>Log in</h1>
-                    <p className='text-gray-600 mb-4 text-xl'>Welcome back! Please enter your details.</p>
+                    <h1 className='text-4xl font-bold mb-7  '>Create an account</h1>
+                    {/* <p className='text-gray-600 mb-4 text-xl'>Welcome back! Please enter your details.</p> */}
                     <form action="" className="w-full max-w-sm">
+                        {/* Name */}
+                        <div className="form-control mb-4">
+                            <label className="label">
+                                <span className="label-text">Name*</span>
+                            </label>
+                            <input
+                                type="text"
+                                placeholder="Enter your Name"
+                                name="name"
+                                className="input input-bordered w-full"
+                                required
+                            />
+                        </div>
                         {/* Email */}
                         <div className="form-control mb-4">
                             <label className="label">
-                                <span className="label-text">Email</span>
+                                <span className="label-text">User Name</span>
+                            </label>
+                            <input
+                                type="text"
+                                placeholder="Enter your unique user name"
+                                name="userName"
+                                className="input input-bordered w-full"
+                                required
+                            />
+                        </div>
+                        {/* Email */}
+                        <div className="form-control mb-4">
+                            <label className="label">
+                                <span className="label-text">Email*</span>
                             </label>
                             <input
                                 type="email"
@@ -45,7 +71,7 @@ const SignUp = () => {
                         {/* Name */}
                         <div className="form-control mb-4">
                             <label className="label">
-                                <span className="label-text">Password</span>
+                                <span className="label-text">Password*</span>
                             </label>
                             <input
                                 type="password"
@@ -55,13 +81,13 @@ const SignUp = () => {
                                 required
                             />
                         </div>
-                        <div className='text-right'>
-                        <button className="btn btn-link no-underline text-[#F88D58] font-thin text-xl my-2">Forgot password</button>
+                        <div className='text-left'>
+                        <h1 className="text-gray-500 font-thin -mt-3  mb-5">Must be least 8 characters.</h1>
                         </div>
                         <button className="btn bg-[#F88D58] hover:bg-black text-white w-full text-xl font-thin">Sign in</button>
                     </form>
                         <button className="btn bg-white text-gray-600 w-full text-xl max-w-sm mt-8 border-gray-300"><FcGoogle className='text-4xl' /> Sign in With Google</button>
-                        <h1 className='text-gray-500'>Don’t have an account?<button className="btn btn-link no-underline text-[#F88D58] font-thin text-xl my-2">sign up</button></h1>
+                        <h1 className='text-gray-500'>Already have an account?<button className="btn btn-link no-underline text-[#F88D58] font-thin  my-2">Log in</button></h1>
                     {/* <GoogleLogin/> */}
                 </div>
             </div>
