dcabe499ea0526ba74718d3097046866b5a835e2
mirza2018 <mirza.eee.4th@gmail.com>
2024-10-19 15:58:03 +0600

    login related all page setup done

--- components/Login/Login.jsx
+++ components/Login/Login.jsx
@@ -4,28 +4,34 @@ import { IoMdArrowBack } from "react-icons/io";
 import loginImg from "../../asserts/login-img.png";
 import logo from "../../asserts/logo.png";
 import { FcGoogle } from "react-icons/fc";
+import Link from 'next/link';
 
 const Login = () => {
     return (
         <div className='container mx-auto pt-5'>
-            {/* Login Back button */}
-            <div className='flex flex-shrink-0 items-center text-xl text-[#656565] gap-4 font-bold'>
+            {/*Back Home button */}
+            <Link href={'/'}>
+           
+            <div className='flex flex-shrink-0 items-center text-xl text-[#656565] gap-4 font-bold cursor-pointer'>
                 <IoMdArrowBack className='text-3xl' />
                 <h1 className=''>Back To Home</h1>
-            </div>
+            </div> 
+            </Link>
             {/* Main content */}
             <div className='flex gap-8'>
                 {/* Left side Image */}
-                <div className='flex flex-col justify-end mt-10 w-1/2 '>
+                <div className=' md:block hidden w-1/2 my-auto'>
+                <div className='flex flex-col justify-end mt-10 '>
                     <Image alt='dog-Image' src={loginImg} className='object-cover aspect-square rounded shadow-xl' />
+                </div>
                 </div>
 
                 {/* Right side login functionality */}
-                <div className='flex-1 flex flex-col justify-center items-center '>
+                <div className='flex-1 flex flex-col justify-center items-center  mx-2'>
                     <div className='mb-5 '>
                         <Image alt='dog-Image' src={logo} className='w-52 ' />
                     </div>
-                    <h1 className='text-4xl font-bold mb-2 mt-16'>Log in</h1>
+                    <h1 className='text-4xl font-bold mb-2 md:mt-16 mt-8'>Log in</h1>
                     <p className='text-gray-600 mb-4 text-xl'>Welcome back! Please enter your details.</p>
                     <form action="" className="w-full max-w-sm">
                         {/* Email */}
@@ -42,7 +48,7 @@ const Login = () => {
                             />
                         </div>
                         {/* Name */}
-                        <div className="form-control mb-4">
+                        <div className="form-control md:mb-4">
                             <label className="label">
                                 <span className="label-text">Password</span>
                             </label>
@@ -55,12 +61,23 @@ const Login = () => {
                             />
                         </div>
                         <div className='text-right'>
-                        <button className="btn btn-link no-underline text-[#F88D58] font-thin text-xl my-2">Forgot password</button>
+                        <Link href={'/login/forgot-password'}>
+                        <button className="btn btn-link no-underline text-[#F88D58] font-thin md:text-xl md:my-2">Forgot password</button>
+                        </Link>
                         </div>
                         <button className="btn bg-[#F88D58] hover:bg-black text-white w-full text-xl font-thin">Sign in</button>
                     </form>
                         <button className="btn bg-white text-gray-600 w-full text-xl max-w-sm mt-8 border-gray-300"><FcGoogle className='text-4xl' /> Sign in With Google</button>
-                        <h1 className='text-gray-500'>Don’t have an account?<button className="btn btn-link no-underline text-[#F88D58] font-thin text-xl my-2">sign up</button></h1>
+                        <Link href={'/sign-up'}>
+                        <h1 className='text-gray-500'>
+                            Don’t have an account?
+                            <button className="btn btn-link no-underline
+                             text-[#F88D58] font-thin md:text-xl my-2">
+                                sign up</button>
+                        </h1>
+                       </Link>
+
+
                     {/* <GoogleLogin/> */}
                 </div>
             </div>
--- components/SignUp/SignUp.jsx
+++ components/SignUp/SignUp.jsx
@@ -4,29 +4,36 @@ import { IoMdArrowBack } from "react-icons/io";
 import loginImg from "../../asserts/Signup-img.png";
 import logo from "../../asserts/logo.png";
 import { FcGoogle } from "react-icons/fc";
+import Link from 'next/link';
 
 
 const SignUp = () => {
     return (
         <div className='container mx-auto pt-5'>
             {/* sign in Back button */}
-            <div className='flex flex-shrink-0 items-center text-xl text-[#656565] gap-4 font-bold'>
-                <IoMdArrowBack className='text-3xl' />
-                <h1 className=''>Back To Home</h1>
-            </div>
+            <Link href={'/'}>
+          
+                <div className='flex flex-shrink-0 items-center text-xl text-[#656565] gap-4 font-bold'>
+                    <IoMdArrowBack className='text-3xl' />
+                    <h1 className=''>Back To Home</h1>
+                </div> 
+            </Link>
             {/* Main content */}
             <div className='flex gap-8 flex-row-reverse'>
                 {/* Right side Image */}
-                <div className='flex flex-col justify-end mt-10 w-1/2 '>
+                <div className='md:block hidden  w-1/2 my-auto'>
+                <div className='flex flex-col justify-end mt-10'>
+
                     <Image alt='dog-Image' src={loginImg} className='object-cover aspect-square rounded shadow-xl' />
+                </div>
                 </div>
 
                 {/* Left side signup functionality */}
-                <div className='flex-1 flex flex-col justify-center items-center '>
+                <div className='flex-1 flex flex-col justify-center items-center mx-2 '>
                     <div className='mb-5 '>
                         <Image alt='dog-Image' src={logo} className='w-52 ' />
                     </div>
-                    <h1 className='text-4xl font-bold mb-7  '>Create an account</h1>
+                    <h1 className='text-4xl font-bold mb-7 text-center flex flex-shrink-0'>Create an account</h1>
                     {/* <p className='text-gray-600 mb-4 text-xl'>Welcome back! Please enter your details.</p> */}
                     <form action="" className="w-full max-w-sm">
                         {/* Name */}
@@ -87,7 +94,16 @@ const SignUp = () => {
                         <button className="btn bg-[#F88D58] hover:bg-black text-white w-full text-xl font-thin">Sign in</button>
                     </form>
                         <button className="btn bg-white text-gray-600 w-full text-xl max-w-sm mt-8 border-gray-300"><FcGoogle className='text-4xl' /> Sign in With Google</button>
-                        <h1 className='text-gray-500'>Already have an account?<button className="btn btn-link no-underline text-[#F88D58] font-thin  my-2">Log in</button></h1>
+                        <h1 className='text-gray-500'>
+                            Already have an account?
+                            <Link href={'/login'}>
+                            <button className="btn btn-link no-underline 
+                            text-[#F88D58] font-thin  my-2">
+                                Log in
+                            </button>
+                            </Link>
+                        </h1>
+
                     {/* <GoogleLogin/> */}
                 </div>
             </div>
